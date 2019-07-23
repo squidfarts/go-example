25f286841c1c55aae1f978f0729a120893196d84
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 16:25:00 -0700

    Add files via upload

---
+++ src/main/main.go
@@ -0,0 +1,27 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.kt                                                                   *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: MIT                                                                    *
+ *                                                                                 *
+ ***********************************************************************************/
+package main
+
+import "fmt"
+
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, GoLang' is desplayed to the standerd console.
+ *
+ */
+func main() {
+    fmt.Println("Hello, GoLang.");
+    
+}// end of function main
